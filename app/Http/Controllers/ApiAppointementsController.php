feed27991f21d8637f15f3ae6a4354afac07e1b6
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-05-01 08:44:11 +0300

    romina

--- app/Http/Controllers/ApiAppointementsController.php
+++ app/Http/Controllers/ApiAppointementsController.php
@@ -14,23 +14,23 @@ class ApiAppointementsController extends Controller
     public function index(Request $r)
     {
         $user_id = ((int)($r->client_id));
+        if($user_id<1){
+            return [];
+        }
 
         $u = User::find($user_id);
         if ($u == null) {
-            return Utils::response([
-                'status' => '0', 
-                'message' => 'Failed. User not found.',
-            ]);
+            return [];
         }
 
         $items = [];
 
-        if($u->user_ty == 'admin'){
+        if ($u->user_ty == 'admin') {
             $items  = Appointment::all();
-        }else if($u->user_ty == 'doctor'){
-            $items  = Appointment::where('doctor_id',$user_id)->get();
-        } else{            
-            $items  = Appointment::where('client_id',$user_id)->get();
+        } else if ($u->user_ty == 'doctor') {
+            $items  = Appointment::where('doctor_id', $user_id)->get();
+        } else {
+            $items  = Appointment::where('client_id', $user_id)->get();
         }
 
 
@@ -42,33 +42,33 @@ class ApiAppointementsController extends Controller
 
         if (
             (!isset($r->product_id)) ||
-            (!isset($r->client_id)) || 
-             (!isset($r->latitude)) ||
+            (!isset($r->client_id)) ||
+            (!isset($r->latitude)) ||
             (!isset($r->longitude)) ||
             (!isset($r->details)) ||
             (!isset($r->category_id))
         ) {
             return Utils::response([
-                'status' => '0', 
+                'status' => '0',
                 'message' => 'No enough data.',
             ]);
         }
- 
+
 
         $user_id = ((int)($r->client_id));
         $product_id = ((int)($r->product_id));
         $u = User::find($user_id);
         if ($u == null) {
             return Utils::response([
-                'status' => '0', 
+                'status' => '0',
                 'message' => 'User not found.',
             ]);
         }
-        
+
         $p = Product::find($product_id);
         if ($p == null) {
             return Utils::response([
-                'status' => '0', 
+                'status' => '0',
                 'message' => 'Service not found..',
             ]);
         }
@@ -86,7 +86,7 @@ class ApiAppointementsController extends Controller
         $ap->appointment_time = '';
         $ap->details = $r->details;
         $ap->order_location = $u->sub_county;
-        
+
 
         if ($ap->save()) {
             return Utils::response([
--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -8,4 +8,39 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     use HasFactory;
-} 
+
+    public function client()
+    {
+        return $this->belongsTo(User::class, 'client_id');
+    }
+    public function doctor()
+    {
+        return $this->belongsTo(User::class, 'doctor_id');
+    }
+    public function hospital()
+    {
+        return $this->belongsTo(Hospital::class, 'hospital_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    protected $with = ['client', 'doctor', 'hospital'];
+    /*
+        "created_at": "2022-04-23T17:30:52.000000Z",
+        "updated_at": "2022-04-23T17:30:52.000000Z",
+        "doctor_id": 1,
+        "status": "Pending",
+        "price": "12000",
+        "latitude": "121.11",
+        "longitude": "0.21",
+        "order_location": null,
+        "": "1",
+        "appointment_time": "",
+        "details": "Simple",
+        
+
+        */
+}
